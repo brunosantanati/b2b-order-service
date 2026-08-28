1c1d91ce0015f48745616766c29d2bcd12cc0d45
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2026-08-28 00:57:27 -0300

    Empty project + Mongo config + docker-compose.yml

---
+++ src/main/java/br/com/vpsconsulting/b2b_order_service/config/MongoConfig.java
@@ -0,0 +1,33 @@
+package br.com.vpsconsulting.b2b_order_service.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "orders_db");
+    }
+}
---
+++ src/test/java/br/com/vpsconsulting/b2b_order_service/B2bOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vpsconsulting.b2b_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
